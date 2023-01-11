1e866adcaa87ce6db272ec1492463466f4f7ccef
Andrei Azarov <Golokron3@yandex.ru>
2023-01-11 21:58:06 +0300

    Ver 0.01.1

---
+++ PJ1_ShootThemUp/Source/PJ1_ShootThemUp/Public/BaseGeometryActor.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseGeometryActor.generated.h"
+
+UCLASS()
+class PJ1_SHOOTTHEMUP_API ABaseGeometryActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseGeometryActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Weapon")
+	int32 WeaponsNum = 4;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Stat")
+	int32 KillsNum = 7;
+
+	UPROPERTY(EditInstanceOnly, Category = "Health")
+	float Health = 34.545352f;
+
+	UPROPERTY(EditAnywhere, Category = "Health")
+	bool IsDead = false;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon")
+	bool HasWeapon = true;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+private:
+	void printTypes();
+	void printStrintTypes();
+
+
+};
